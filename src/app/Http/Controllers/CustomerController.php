11b120b660a917ea2a9b6c344d39f97ac007fff3
basudev-g <flancer.basudev@gmail.com>
2026-01-21 23:49:09 +0600

    customer module code updated

--- src/app/Http/Controllers/CustomerController.php
+++ src/app/Http/Controllers/CustomerController.php
@@ -2,25 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Resources\CustomerResource;
 use App\Models\Customer;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
+    use AuthorizesRequests;
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $customers = Customer::paginate(15);
+
+        return CustomerResource::collection($customers);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCustomerRequest $request)
     {
-        //
+        $customer = Customer::create($request->validated());
+
+        return response()->json([
+            'message' => 'Customer created successfully',
+            'data'    => new CustomerResource($customer),
+        ], 201);
     }
 
     /**
@@ -28,15 +41,22 @@ class CustomerController extends Controller
      */
     public function show(Customer $customer)
     {
-        //
+        $this->authorize('view', $customer);
+
+        return new CustomerResource($customer);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Customer $customer)
+    public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->validated());
+
+        return response()->json([
+            'message' => 'Customer updated successfully',
+            'data'    => new CustomerResource($customer),
+        ]);
     }
 
     /**
@@ -44,6 +64,12 @@ class CustomerController extends Controller
      */
     public function destroy(Customer $customer)
     {
-        //
+        $this->authorize('delete', $customer);
+
+        $customer->delete();
+
+        return response()->json([
+            'message' => 'Customer deleted successfully'
+        ]);
     }
 }
